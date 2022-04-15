0341c242a547f470c2c70cf116cf3e9f3f118c29
adawolfs <adawolfs@gmail.com>
2022-04-15 14:36:23 +0000

    implement rive

---
+++ js/guategeeks/rive/main.js
@@ -0,0 +1,10 @@
+
+// Background animation
+new rive.Rive({
+    src: "rive/guategeeks_page_background.riv",
+    // Or the path to a local Rive asset
+    // src: './example.riv',
+    canvas: document.getElementById("background-canvas"),
+    artboard: "background",
+    autoplay: true
+});
