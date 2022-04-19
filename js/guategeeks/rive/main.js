c36ccd772f95d160f433e6e6e5e70edf0c438266
adawolfs <adawolfs@gmail.com>
2022-04-19 04:01:21 +0000

    Implement rive animations

--- js/guategeeks/rive/main.js
+++ js/guategeeks/rive/main.js
@@ -3,10 +3,19 @@
 let canvas = document.getElementsByClassName("background-canvas");
 for (let i = 0; i < canvas.length; i++) {
     new rive.Rive({
-        src: "rive/guategeeks_page_background.riv",
+        src: "assets/rive/guategeeks_page_background.riv",
         canvas: canvas[i],
         artboard: "background",
         autoplay: true
     });
 }
 
+
+let laptop_pc_phone = document.getElementsByClassName("rive-laptop-pc-phone")[0];
+new rive.Rive({
+    src: "assets/rive/laptop-pc-phone.riv",
+    canvas: laptop_pc_phone,
+    animations: ["pc", "phone", "pc-laptop", "laptop-pc", "phone-laptop", "pc-light"],
+    artboard: "composition",
+    autoplay: true
+});
