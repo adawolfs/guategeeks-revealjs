514d97eca4ad3a51ef0b3b0fa62c512d7e4d351f
adawolfs <adawolfs@gmail.com>
2022-04-30 19:06:18 +0000

    Start rive animations when the slide is active.

--- js/guategeeks/rive/main.js
+++ js/guategeeks/rive/main.js
@@ -2,6 +2,7 @@
 // Background animation
 console.log(guategeeks)
 guategeeks.waitForReady('background');
+let riveComponents = [];
 let rive_background = new rive.Rive({
     src: "assets/rive/guategeeks_page_background.riv",
     canvas: document.getElementsByClassName("background-canvas")[0],
@@ -14,6 +15,7 @@ rive_background.on('load', () => {
     guategeeks.isReady('background');
 });
 
+riveComponents.push(rive_background);
 
 guategeeks.waitForReady('rive_about_us');
 let rive_about_us = new rive.Rive({
@@ -28,6 +30,8 @@ rive_about_us.on('load', () => {
     guategeeks.isReady('rive_about_us');
 });
 
+riveComponents.push(rive_about_us);
+
 guategeeks.waitForReady('rive_rive_ai');
 let rive_ai = new rive.Rive({
     src: "assets/rive/ai.riv",
@@ -39,6 +43,8 @@ let rive_ai = new rive.Rive({
 rive_ai.on('load', () => {
     guategeeks.isReady('rive_rive_ai');
 });
+
+riveComponents.push(rive_ai);
 
 
 guategeeks.waitForReady('rive_blog');
@@ -54,6 +60,7 @@ rive_blog.on('load', () => {
     guategeeks.isReady('rive_blog');
 })
 
+riveComponents.push(rive_blog);
 
 guategeeks.waitForReady('rive_robot');
 let rive_robot = new rive.Rive({
@@ -67,3 +74,31 @@ let rive_robot = new rive.Rive({
 rive_robot.on('load', () => {
     guategeeks.isReady('rive_robot');
 });
+
+riveComponents.push(rive_robot);
+
+// Listeners for the animations
+Reveal.on( 'slidechanged', event => {
+    
+    console.log(event)
+    riveComponents.forEach(riveComponent => {
+        console.log(riveComponent);
+        riveComponent.pause();
+    });
+    
+    if (event.currentSlide === document.querySelector('.guategeeks-course-ai')) {
+        rive_ai.play(["brain", "data1"]);
+    } 
+
+    if (event.currentSlide === document.querySelector('.guategeeks-course-robotics')) {
+        rive_robot.play(["lid", "particles", "floating"]);
+    }
+
+    if (event.currentSlide === document.querySelector('.guategeeks-about-us')) {
+        rive_about_us.play(["pc", "phone", "pc-laptop", "laptop-pc", "phone-laptop", "pc-light"]);
+    }
+    if (event.currentSlide === document.querySelector('.guategeeks-blog')) {
+        rive_blog.play(["image", "chat", "hearth", "face", "pencil", "load"]);
+    }
+    // event.previousSlide, event.currentSlide, event.indexh, event.indexv
+} );
